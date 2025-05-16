4b0140e435c16bd284cfccf74f763c60d46ae94a
YoloBox164 <szucs.mate513@gmail.com>
2025-05-16 15:02:13 +0200

    transparent neo-tree
    figured out how to remove neo-tree's bg, will do tablines next

--- init.lua
+++ init.lua
@@ -105,5 +105,11 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- [[ Fully fledged transparency ]]
+vim.cmd [[
+  highlight NeoTreeNormal guibg=none
+  highlight NeoTreeNormal ctermbg=none
+]] -- quick fix, I can't get the tabline to be transparent so the solid background stays for now
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
